ab857d27e195c83c70b2ca3c49abe9bb840815a8
unknown <dasilvadeoliveirafabio@gmail.com>
2022-12-06 00:33:06 -0300

    teste de deploy 3

--- models/prod.js
+++ models/prod.js
@@ -1,7 +1,10 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+require('dotenv').config()
+module.exports =  Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.USER_DB,process.env.BANC_DB,process.env.PASSWORD_DB,{
+  host:process.env.HOST_DB,
+  dialect:'mysql',
+  dialectModule: require('mysql2')
+});
 module.exports = (sequelize, DataTypes) => {
   const Prod = sequelize.define('Prod',{
     nome:DataTypes.STRING
