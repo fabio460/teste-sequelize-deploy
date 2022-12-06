f126a33f09d05e7d349a9ab31c2b0651837903a7
unknown <dasilvadeoliveirafabio@gmail.com>
2022-12-06 08:03:49 -0300

    teste de deploy 5

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 require('dotenv').config()
-const {Prod} = require('./models')
+const {Prod,roupa} = require('./models')
 const app = express()
 app.use(express.json())
 app.get('/',async(req,res)=>{
-    const p = await Prod.findAll()
+    const p = await roupa.findAll()
     res.send(p)
 })
 
 app.post('/create',async(req,res)=>{
     const {nome} = req.body
-    const p = await Prod.create({
+    const p = await roupa.create({
        nome
     })
     res.json(p)
--- models/prod.js
+++ models/prod.js
@@ -1,13 +1,9 @@
-require('dotenv').config()
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.USER_DB,process.env.BANC_DB,process.env.PASSWORD_DB,{
-  host:process.env.HOST_DB,
-  dialect:'mysql',
-  dialectModule: require('mysql2')
-});
+const {sequelize,Sequelize} = require("../connect");
+
 module.exports = ( DataTypes) => {
   const Prod = sequelize.define('Prod',{
     nome:{type:Sequelize.STRING}
   })
+  
   return Prod;
 };
