0d8a2b8ac24437499992df36736a97cdda6f9811
unknown <dasilvadeoliveirafabio@gmail.com>
2022-12-07 17:41:42 -0300

    testando api producao 15

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 require('dotenv').config()
-const motorista = require('./models/motorista')
+const {motorista} = require('./models')
 
 
 const app = express()
 app.use(express.json())
 app.get('/',async(req,res)=>{
-    
+
     const p = await motorista.findAll()
     res.send(p)
 })
