65e1a86d3d3bf17807be71345cf325a6db39e83d
unknown <dasilvadeoliveirafabio@gmail.com>
2022-12-07 11:07:33 -0300

    testando api producao 8

--- index.js
+++ index.js
@@ -2,42 +2,13 @@ const express = require('express')
 require('dotenv').config()
 const {Veiculo,motorista} = require('./models')
 
+
 const app = express()
 app.use(express.json())
 app.get('/',async(req,res)=>{
-
-    const v = await motorista.findAll({
-        
-    })
+    const v = await motorista.findAll()
     res.send(v)
 })
 
-
-
-
-app.post('/createVeiculo',async(req,res)=>{
-    const {nome,id_motorista} = req.body
-    const p = await Veiculo.create({
-       nome,id_motorista
-    })
-    res.json(p)
-})
-app.post('/createMotorista',async(req,res)=>{
-    const {nome} = req.body
-    const p = await motorista.create({
-       nome
-    })
-    res.json(p)
-})
-
-app.delete('/deleteMotorista',async(req,res)=>{
-    const {id} = req.body
-    const d = await Veiculo.destroy({
-        where: {
-          id
-        }
-      });
-    res.json(d)  
-})
 
 app.listen(4000,()=>console.log('rodando...'))
