330686bdd28a83771411d8bf6cadfeb8cd1c7614
unknown <dasilvadeoliveirafabio@gmail.com>
2022-12-07 23:26:49 -0300

    testando api producao 17

--- index.js
+++ index.js
@@ -8,8 +8,17 @@ const app = express()
 app.use(express.json())
 app.get('/',async(req,res)=>{
     const v = await veiculo.findAll()
-    const p = await motorista.findAll()
-    res.send(v)
+    const p = await motorista.findAll({
+        include:veiculo
+    })
+    res.send(p)
 })
 
+app.post('/create',async (req,res)=>{
+   const {nome,id_motorista} = req.body 
+   const r = await veiculo.create({
+     nome,id_motorista
+   })
+   res.send(r)
+})
 app.listen(4000,()=>console.log('rodando...'))
